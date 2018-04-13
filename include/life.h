c6059707ae4e496b99cc1bbf9bea8ddc74fd7b2e
Pablo Emanuell Lopes Targino <pabloemanuell@academico.ufrn.br>
2018-04-12 22:56:51 -0300

    Add Life methods, removing character propriety and reorganized header

--- include/life.h
+++ include/life.h
@@ -2,66 +2,41 @@
 #define LIFE_H
 
 #include <iostream>
+#include <vector>
 
 enum Cell{
 	CELL_ALIVE = 1,
 	CELL_DIED = 0
 };
-class Life
-{
+
+class Life {
 	private:
-		int** grid;
+		std::vector<std::vector<int>> grid;
 		int rows;
-		int columns;
-		char caractere;
-
+		int cols;
+		//TODO: identidade da grade
 	public:
-
-	Life(int nLin = 20, int nCol = 60){
-		this->rows = nLin;
-		this->columns = nCol;
-	}
-
-	Life( int nLin = 20, int nCol = 60, char caract = '*' ){
-		this->rows = nLin;
-		this->columns = nCol;
-		this->caractere = caract;
-	}
-
-	//set_alive();
-	//update();
-	//print();
+	/// construtor
+	Life(int nLin, int nCol);
+	Life::~Life();
+	///destructor
+	/// sobrecarga do operador =
+	Life & operator=(const Life& lf);
+	bool operator==(const Life& lf) const; 
+	//set_alive();	//update();
+	/// imprimi a grade
+	void print();
 	//stable();
-	//extinct();
+	//extinct
 	// sobrecarga de operadores
-
 	// Métodos de recuperação de valores
-
 	/// Retorna a quantidade de linhas da matriz
 	int get_rows(){
 		return this->rows;
 	}
-
-	void set_rows( int row ){
-		this->rows = row;
-	}
-
 	/// Retorna a quantidade de colunas da matriz
 	int get_columns(){
-		return this->columns;
-	}
-
-	void set_columns( int col ){
-		this->columns = col;
-	}
-
-	/// Retorna o caractere utilizado para identificar as celulas vivas
-	char get_caractere(){
-		return this->caractere;
-	}
-
-	void set_caractere( char carac ){
-		this->caractere = carac;
+		return this->cols;
 	}
 };
 #endif
--- src/Life.cpp
+++ src/Life.cpp
@@ -1,31 +1,57 @@
 #include "life.h"
 
-enum Cell{
-	CELL_ALIVE = 1,
-	CELL_DIED = 0
-};
-class Life
+Life::Life(int nLin = 20, int nCol = 60)
 {
-	private:
-		std::vector<std::vector<int>> grid;
-	public:
-		Life(int nLin, int nCol)
+	this->rows = nLin;
+	this->rows = nCol;
+	this->grid.resize(nLin, {{0}});
+	for(int i = 0; i < nLin; ++i)
+	{
+		this->grid[i].resize(nCol, 0);
+		for(int j = 0; j < nCol; ++j)
 		{
-			grid.resize(nLIn);
-			for(int i = 0; i < nLin; ++i)
-			{
-				grid[i].resize[nCol];
-				for(int j = 0; j < nCol; ++j)
-				{
-					grid[i][j] = 0;
-				}
-			}
-		
+			this->grid[i][j] = 0;
 		}
-		set_alive();
-		update();
-		print();
-		stable();
-		extinct();
-		// sobrecarga de operadores
+	}
+
 }
+Life::~Life(){
+	this->grid.resize(0);
+	for(int i = 0; i < this->rows; ++i)
+		this->grid[i].resize(0);
+}
+//set_alive();
+//update();
+void Life::print(){
+	for(int i = 0; i < this->rows; ++i){
+		for(int j = 0; j < this->cols; ++j){
+			if(this->grid[i][j] == CELL_ALIVE)
+				std::cout << '*';
+			else
+				std::cout << ' ';
+		}
+		std::cout << std::endl;
+	}
+}
+//stable();
+//extinct();
+Life & Life::operator=(const Life& lf) {
+	this->rows = lf.rows;					// !
+	this->cols = lf.cols;					// iguila propriedades
+	// TODO: igualar a identidade
+	this->grid.resize(lf.rows);	 			// altera quantidade de linhas
+	for(int i = 0; i < lf.rows; ++i){ 			// percorre cada linha
+		this->grid[i].resize(lf.cols);			// redimensiona linha
+		for(int j = 0; j < lf.cols; ++j){		// percorres as colunas
+			this->grid[i][j] = lf.grid[i][j]; 	// realiza a atribuição
+		}
+	}	
+	return *this; 						// permite atribuição em cascata 
+}
+/// sobrecarga do operador ==
+bool Life::operator==(const Life& lf) const {
+	if(lf==lf) // warning chato
+	// TODO: usar comparação por hashing ou algo similar 
+	return true;
+}
+
