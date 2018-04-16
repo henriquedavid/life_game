7e5591e01b2fd2961a01453c8bb473cb0af4e1ab
Henrique David <henriquemed101@gmail.com>
2018-04-16 11:10:03 -0300

    Making the read in commands arguments.

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <cstring>
 #include <iomanip>
+#include <string>
 
 using palavra = std::string;
 using arq_entrada = std::ifstream;
@@ -9,9 +10,18 @@ using arq_entrada = std::ifstream;
 #include "../include/life.h"
 #include "../include/inicial.h"
 
-int main(int argc, char * args[]){
+int main( int argc, char * argv[] ){
 	
- 	Life lf(20, 60, "-");
+	std::string arquivo;
+
+	if(argc != 2){
+		std::cout << "Wrong syntaxe!\n"
+				  << "Use: life <input_cfg_file> [<output_cfg_evolution_file>]" << std::endl;
+		return 0;
+	}
+
+
+ 	Life lf(20, 60, '-');
  	initial(lf, "TESTE");
 
 	return 0;
