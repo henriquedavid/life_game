8fc2efcb7e7453227975b5d82e3670d1d2f3eb0c
Henrique David <henriquemed101@gmail.com>
2018-04-16 11:45:17 -0300

    Updating Life.cpp like adaptar_codigo branch and updating caracter.

--- src/main.cpp
+++ src/main.cpp
@@ -14,16 +14,20 @@ int main( int argc, char * argv[] ){
 	
 	std::string arquivo;
 
+	/// Verifica se quantidade de argumentos.
 	if(argc != 2){
 		std::cout << "Wrong syntaxe!\n"
 				  << "Use: life <input_cfg_file> [<output_cfg_evolution_file>]" << std::endl;
 		return 0;
 	}
 
+	/// O segundo argumento é arquivo.
 	arquivo = argv[1];
 
-
+	/// Inicializa o objeto com a configuração padrão.
  	Life lf(20, 60, '-');
+
+ 	/// Chama o processo de leitura e inicio do life.	
  	initial(lf, arquivo);
 
 	return 0;
