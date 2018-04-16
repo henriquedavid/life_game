3bf0e4b3557e67c3a16a93efc424d8fec11fc2a0
Henrique David <henriquemed101@gmail.com>
2018-04-16 14:48:17 -0300

    The method readConfig add data in the Life's class vector and verify if everything it's okay with the configuration in the configuration default file.

--- include/readFile.h
+++ include/readFile.h
@@ -11,6 +11,60 @@ void readConfig(Life &lf, arq_entrada &arquivo){
 		lf.set_rows(ro);
 		lf.set_columns(col);
 		lf.set_caractere(carac);
+		break;
 	}
 
+	char valor;
+	std::vector<int> valores;
+
+	/// Filtra os elementos para dentro de um vetor.
+	while( arquivo.get(valor) ){
+		if(valor == '.')
+			valores.push_back(0);
+		if(valor == '*')
+			valores.push_back(1);
+	}
+
+	/// Verifica se o tamanho e a quantidade de elementos estão certos.
+	unsigned int tamanho = ro * col;
+
+	if( tamanho != valores.size()){
+		std::cout << "Há células faltantes na configuração inicial!" << std::endl;
+		exit(0);
+	}
+
+	int ax = 0;
+	for( auto i(0) ; i < ro ; i++ ){
+		std::vector<int> v;
+		for(auto j(0) ; j < col ; j++ ){
+			v.push_back(valores[ax]);
+			ax++;
+		}
+		lf.add_data_vector(v);
+	}
+	
+}
+
+/// Verifica se o usuário ainda deseja gerar mais células.
+bool print_continue(){
+	char answer;
+
+	/// Recebe a reposta do usuário.
+	std::cout << "Continue viewing new generations?\n(y,n)? ";
+	std::cin >> answer;
+
+	std::cout << std::endl;
+
+	/// Retorna a resposta do usuário.
+	if( answer == 'y')
+		return true;
+	else if( answer == 'n')
+		return false;
+	else{
+		/// Verifica se a resposta é válida.
+		std::cout << "Sorry, we didn't understand!\n" << std::endl;
+		print_continue();
+	}
+
+	return true;
 }
