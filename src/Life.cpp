f08b611eb3eebd08e18842b11686a74a7a477310
Pablo Emanuell Lopes Targino <pabloemanuell@A307-02L.imd.local>
2018-04-17 07:14:28 -0300

    fixed save_file call

--- src/Life.cpp
+++ src/Life.cpp
@@ -23,19 +23,19 @@ Life::~Life()
 //set_alive();
 //update();
 /*
-void Life::print(){
-	for(int i = 0; i < this->rows; ++i)
-    {
-		for(int j = 0; j < this->cols; ++j)
-        {
-			if(this->grid[i][j] == CELL_ALIVE)
-				std::cout << this->characte;
-			else
-				std::cout << ' ';
-		}
-		std::cout << std::endl;
-	}
-}*/
+   void Life::print(){
+   for(int i = 0; i < this->rows; ++i)
+   {
+   for(int j = 0; j < this->cols; ++j)
+   {
+   if(this->grid[i][j] == CELL_ALIVE)
+   std::cout << this->characte;
+   else
+   std::cout << ' ';
+   }
+   std::cout << std::endl;
+   }
+   }*/
 //stable();
 //extinct();
 Life & Life::operator=(const Life& lf) 
@@ -44,10 +44,10 @@ Life & Life::operator=(const Life& lf)
 	this->cols = lf.cols;					    // | iguala propriedades
 	this->grid.resize(lf.rows);	 			    // altera quantidade de linhas
 	for(int i = 0; i < lf.rows+2; ++i) 			// percorre cada linha
-    {
+	{
 		this->grid[i].resize(lf.cols+2);			// redimensiona linha
 		for(int j = 0; j < lf.cols+2; ++j)		// percorred as colunas
-        {
+		{
 			this->grid[i][j] = lf.grid[i][j]; 	// realiza a atribuição
 		}
 	}	
@@ -57,104 +57,104 @@ Life & Life::operator=(const Life& lf)
 }
 bool Life::operator==(const Life& lf) const 
 {
-    int n = this->id[0];            // number of elements: number of distances + start_x + start_y + distance_1 + ... + distance_n
-    for(int i = 0; i < n+3; ++i)         // if the sizes are different the loop will break at first time
-    {
-        if(this->id[i] != lf.id[i]) // different data
-            return false;
-    }
+	int n = this->id[0];            // number of elements: number of distances + start_x + start_y + distance_1 + ... + distance_n
+	for(int i = 0; i < n+3; ++i)         // if the sizes are different the loop will break at first time
+	{
+		if(this->id[i] != lf.id[i]) // different data
+			return false;
+	}
 	return true;
 }
 void Life::set_id()
 {
-    int n = 0;
-    int distance = 0;
-    bool first_pos = true;
-    this->id.reserve(3);
-    
+	int n = 0;
+	int distance = 0;
+	bool first_pos = true;
+	this->id.reserve(3);
+
 	for(int i = 0; i < this->rows; ++i)       // percorre cada linha    
-    { 			
+	{ 			
 		for(int j = 0; j < this->cols; ++j)   // percorre as colunas
-        {
+		{
 			if(grid[i][j] == CELL_ALIVE)
-            {
-                if(first_pos)              // aren't start_x and start_y stored?
-                {
-                    this->id[1] = i;       // store start_x position
-                    this->id[2] = j;       // store start_y position
-                    first_pos = false;     // start_x and start_y stored
-                }
-                else                       // start coords are stored 
-                {
-                    this->id.push_back(distance); // store new distance
-                    n++;                          // increase number of distances count
-                }
-                distance = 0;                     // reset distance
-            }
-            else
-            {
-                distance++; // increase distance count
-            }
+			{
+				if(first_pos)              // aren't start_x and start_y stored?
+				{
+					this->id[1] = i;       // store start_x position
+					this->id[2] = j;       // store start_y position
+					first_pos = false;     // start_x and start_y stored
+				}
+				else                       // start coords are stored 
+				{
+					this->id.push_back(distance); // store new distance
+					n++;                          // increase number of distances count
+				}
+				distance = 0;                     // reset distance
+			}
+			else
+			{
+				distance++; // increase distance count
+			}
 		}
 	}
 	this->id[0] = n;
 }
 
 void Life::add_data_vector(std::vector<int> vetor){
-    grid.push_back(vetor);
+	grid.push_back(vetor);
 }
 
 /// Imprime para o usuário como está a organização das células.
 void Life::print_data_vector(int numero_geracao){
-    std::cout << "Showing generation " << numero_geracao << ":\n";
-    for( auto i(1) ; i < this->rows+1 ; i++ ){
-        std::cout << "[ ";
-        for( auto j(1) ; j < this->cols+1 ; j++ ){
-            if( grid[i][j] == 1)
-                std::cout << this->characte;
-            else
-                std::cout << " ";
-        }
-        std::cout << "]" <<std::endl;
-    }
+	std::cout << "Showing generation " << numero_geracao << ":\n";
+	for( auto i(1) ; i < this->rows+1 ; i++ ){
+		std::cout << "[ ";
+		for( auto j(1) ; j < this->cols+1 ; j++ ){
+			if( grid[i][j] == 1)
+				std::cout << this->characte;
+			else
+				std::cout << " ";
+		}
+		std::cout << "]" <<std::endl;
+	}
 }
 
 int Life::get_value( int x, int y ){
-    return this->grid[x][y];
+	return this->grid[x][y];
 }
 
 void Life::set_value( int x, int y, int value ){
-    this->grid[x][y] = value;
+	this->grid[x][y] = value;
 }
 
 int Life::get_rows(){
-    return this->rows;
+	return this->rows;
 }
 /// Retorna a quantidade de colunas da matriz
 int Life::get_columns(){
-    return this->cols;
+	return this->cols;
 }
 
 void Life::set_rows(int row){
-    this->rows = row;
+	this->rows = row;
 }
 
 void Life::set_columns(int column){
-    this->cols = column;
+	this->cols = column;
 }
 
 char Life::get_caractere(){
-    return this->characte;
+	return this->characte;
 }
 
 void Life::set_caractere(char carac){
-    this->characte = carac;
+	this->characte = carac;
 }
 
 bool Life::is_extinct(){
-    if(this->id[0] == 0)
-        return true;
-    return false;
+	if(this->id[0] == 0)
+		return true;
+	return false;
 }
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -19,28 +19,28 @@ using arq_saida = std::ofstream;
 //#include <istream>
 
 int main( int argc, char * argv[] ){
-	
+
 	std::string arquivo;
-    std::string arquivo_saida;
+	std::string arquivo_saida;
 	/// Verifica se quantidade de argumentos.
 	if(argc < 2){
 		std::cout << "Wrong syntaxe!\n"
-				  << "Use: life <input_cfg_file> [<output_cfg_evolution_file>]" << std::endl;
+			<< "Use: life <input_cfg_file> [<output_cfg_evolution_file>]" << std::endl;
 		return 0;
 	}
 
 	/// O segundo argumento é arquivo.
 	arquivo = argv[1];
-    if(argc == 3)
-    {
-        arquivo_saida = argv[2];
-    }
+	if(argc == 3)
+	{
+		arquivo_saida = argv[2];
+	}
 	/// Inicializa o objeto com a configuração padrão.
- 	Life lf(20, 60, '-');
- 	Warehouse ware;	
+	Life lf(20, 60, '-');
+	Warehouse ware;	
 
- 	/// Chama o processo de leitura e inicio do life.	
- 	initial(lf, arquivo);
+	/// Chama o processo de leitura e inicio do life.	
+	initial(lf, arquivo);
 
 	lf.print_data_vector();
 
@@ -48,13 +48,13 @@ int main( int argc, char * argv[] ){
 		update(lf, ware);
 		lf.print_data_vector(ware.get_size()+1);
 		//verifica_estavel();
-        
+
 		if(lf.is_extinct()){
-           std::cout << ">>> All Cells are extinct. Exiting...\n";
-           return 0;
-        }
+			std::cout << ">>> All Cells are extinct. Exiting...\n";
+			break;
+		}
 	}
 	if(argc == 3)
-        save_file(ware, arquivo_saida);
+		save_file(ware, arquivo_saida);
 	return 0;
 }
