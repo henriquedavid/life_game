1cc6c369bb9e63d198039c83bdb2e892c5cc4930
Pablo Emanuell Lopes Targino <pabloemanuell@A307-02L.imd.local>
2018-04-19 10:45:08 -0300

    arumando o codigo

--- src/Life.cpp
+++ src/Life.cpp
@@ -7,14 +7,10 @@ Life::Life(int nLin = 20, int nCol = 60)
 {
 	this->rows = nLin;
 	this->rows = nCol;
-	this->grid.resize(nLin, {{CELL_DIED}});
-	for(int i = 0; i < nLin; ++i)
+	this->grid.resize(nLin+2, {{CELL_DIED}}); // aloca linhas com valor padrao
+	for(int i = 0; i < nLin+2; ++i) // percorre linhas
 	{
-		this->grid[i].resize(nCol, CELL_DIED);
-		for(int j = 0; j < nCol; ++j)
-		{
-			this->grid[i][j] = CELL_DIED;
-		}
+		this->grid[i].resize(nCol+2, CELL_DIED); // aloca colunas da linha com valor padrao
 	}
 
 }
@@ -33,66 +29,17 @@ Life & Life::operator=(const Life& lf)
 {
 	this->rows = lf.rows;					    // !
 	this->cols = lf.cols;					    // | iguala propriedades
-	this->grid.resize(lf.rows);	 			    // altera quantidade de linhas
-	for(int i = 0; i < lf.rows; ++i) 			// percorre cada linha
+	this->grid.resize(lf.rows+2);	 			    // altera quantidade de linhas
+	for(int i = 0; i < lf.rows+2; ++i) 			// percorre cada linha
 	{
-		this->grid[i].resize(lf.cols);			// redimensiona linha
-		for(int j = 0; j < lf.cols; ++j)		// percorred as colunas
+		this->grid[i].resize(lf.cols+2);			// redimensiona linha
+		for(int j = 0; j < lf.cols+2; ++j)		// percorred as colunas
 		{
 			this->grid[i][j] = lf.grid[i][j]; 	// realiza a atribuição
 		}
-	}	
-	this->set_id();                             // seta Vetor de identidade
+	}	                            		// seta Vetor de identidade
 	return *this; 						        // permite atribuição em cascata 
 	// TODO: traduzir comentarios
-}
-
-/// Operador de igualdade da classe life.
-/// \param Classe Life.
-bool Life::operator==(const Life& lf) const 
-{
-	int n = this->id[0];            // number of elements: number of distances + start_x + start_y + distance_1 + ... + distance_n
-	for(int i = 0; i < n+3; ++i)         // if the sizes are different the loop will break at first time
-	{
-		if(this->id[i] != lf.id[i]) // different data
-			return false;
-	}
-	return true;
-}
-
-void Life::set_id()
-{
-	int n = 0;
-	int distance = 0;
-	bool first_pos = true;
-	this->id.reserve(3);
-
-	for(int i = 0; i < this->rows; ++i)       // percorre cada linha    
-	{ 			
-		for(int j = 0; j < this->cols; ++j)   // percorre as colunas
-		{
-			if(grid[i][j] == CELL_ALIVE)
-			{
-				if(first_pos)              // aren't start_x and start_y stored?
-				{
-					this->id[1] = i;       // store start_x position
-					this->id[2] = j;       // store start_y position
-					first_pos = false;     // start_x and start_y stored
-				}
-				else                       // start coords are stored 
-				{
-					this->id.push_back(distance); // store new distance
-					n++;                          // increase number of distances count
-				}
-				distance = 0;                     // reset distance
-			}
-			else
-			{
-				distance++; // increase distance count
-			}
-		}
-	}
-	this->id[0] = n;
 }
 
 /// Adiciona dados ao vetor da classe Grid na classe Life.
@@ -169,9 +116,13 @@ void Life::set_caractere(char carac){
 /// Verificar se alguma geração está extinta.
 /// \return retorno do estado da geração.
 bool Life::is_extinct(){
-	if(this->id[0] == 0)
-		return true;
-	return false;
+	for( auto i(1) ; i < this->rows+1 ; i++ ){
+		for( auto j(1) ; j < this->cols+1 ; j++ ){
+			if(this->grid[i][j] == CELL_ALIVE)
+				return false;
+		}
+	}
+	return true;
 }
 
 
